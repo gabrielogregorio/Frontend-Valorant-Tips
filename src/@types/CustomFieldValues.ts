34a9c42cc9ff94cc0991c8b87cd8159e9eb7b460
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2025-01-12 10:33:53 -0300

    feat: added input tag form

--- src/@types/CustomFieldValues.ts
+++ src/@types/CustomFieldValues.ts
@@ -1,3 +1,5 @@
-export type CustomFieldValues = { [x: string]: string | number | boolean | string[] | number[] | boolean };
-export type CustomFieldValuesBoolean = { [x: string]: boolean  };
+import { InputTagBaseValue } from './InputTagBaseValue';
 
+export type CustomFieldValues = { [x: string]: string | number | boolean | string[] | number[] | boolean };
+export type CustomFieldValuesBoolean = { [x: string]: boolean };
+export type CustomFieldValuesInputTagBaseValue = { [x: string]: InputTagBaseValue };
